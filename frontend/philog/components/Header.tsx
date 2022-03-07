13494ff8cafb53b28479bd55e9a38e32dbe7c61b
gofeel8 <gofeel8@hanmail.net>
2022-03-08 00:23:48 +0900

    Refact: Navbar 컴포넌트 로직 분리

--- frontend/philog/components/Header.tsx
+++ frontend/philog/components/Header.tsx
@@ -1,36 +1,25 @@
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
-import { MediaSize, Color, HeaderHeight, PageObj } from "../utils/constant";
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { MediaSize, HeaderHeight } from "../utils/constant";
+import { Dispatch, SetStateAction, useState } from "react";
 import Link from "next/link";
-import NavTab from "./NavTab";
-import { useNavIdx } from "../hooks/useNavIdx";
+import Navbar from "./Navbar";
 interface HeaderProps {
   setDarkMode: Dispatch<SetStateAction<boolean>>;
   isDarkMode: boolean;
 }
 
-export default function Navbar({ setDarkMode, isDarkMode }: HeaderProps) {
+export default function Header({ setDarkMode, isDarkMode }: HeaderProps) {
   const [showNav, setShowNav] = useState(false);
-  const navIdx = useNavIdx();
 
   return (
     <>
-      <Header>
+      <HeaderContainer>
         <Link href="/" passHref={true}>
           <Logo>Philog</Logo>
         </Link>
-        <NavContainer>
-          <NavList toggle={showNav}>
-            <NavTab title={PageObj.Profile}></NavTab>
-            <NavTab title={PageObj.Tech}></NavTab>
-            <NavTab title={PageObj.Photo}></NavTab>
-            <NavTab title={PageObj.Diet}></NavTab>
-            <NavTab title={PageObj.Login}></NavTab>
-          </NavList>
-          <Indicator navIdx={navIdx} />
-        </NavContainer>
+        <Navbar showNav={showNav}></Navbar>
         <BtnContainer>
           <ThemeBtn onClick={() => setDarkMode((prev) => !prev)}>
             <FontAwesomeIcon icon={isDarkMode ? faSun : faMoon} />
@@ -39,7 +28,7 @@ export default function Navbar({ setDarkMode, isDarkMode }: HeaderProps) {
             <FontAwesomeIcon icon={faBars} />
           </Button>
         </BtnContainer>
-      </Header>
+      </HeaderContainer>
     </>
   );
 }
@@ -48,7 +37,7 @@ const BtnContainer = styled.div`
   display: flex;
 `;
 
-const Header = styled.header`
+const HeaderContainer = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -64,48 +53,6 @@ const Header = styled.header`
 const Logo = styled.a`
   @media only screen and (max-width: ${MediaSize}px) {
     text-align: center;
-  }
-`;
-
-const NavContainer = styled.div`
-  flex-grow: 1;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-`;
-
-const Indicator = styled.div<{ navIdx: number }>`
-  width: 80px;
-  display: ${({ navIdx }) => (navIdx >= 0 ? "block" : "none")};
-  margin-right: ${({ navIdx }) =>
-    navIdx >= 0 && `calc(320px - 80px * ${navIdx});`};
-  border: 1px solid ${Color.lightGray};
-  transition: all ease 0.5s 0s;
-  @media only screen and (max-width: ${MediaSize}px) {
-    display: none;
-  }
-`;
-
-const NavList = styled.ul<{ toggle: boolean }>`
-  justify-content: space-between;
-  display: flex;
-  align-items: flex-end;
-  width: 400px;
-  margin: 0;
-  padding: 0;
-  @media only screen and (max-width: ${MediaSize}px) {
-    display: ${({ toggle }) => (toggle ? "block" : "none")};
-    position: absolute;
-    top: ${HeaderHeight}px;
-    left: 0;
-    margin: 0;
-    background-color: white;
-    border-bottom: 1px solid black;
-    border-radius: 0 0 20px 20px;
-    width: 100%;
-    padding: 0;
-    background-color: ${(props) => props.theme.primary};
-    border-bottom: 1px solid ${(props) => props.theme.secondary};
   }
 `;
 
---
+++ frontend/philog/components/Navbar.tsx
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+import { useNavIdx } from "../hooks/useNavIdx";
+import { Color, HeaderHeight, MediaSize, PageObj } from "../utils/constant";
+import NavTab from "./NavTab";
+
+interface NavbarProps {
+  showNav: boolean;
+}
+
+export default function Navbar({ showNav }: NavbarProps) {
+  const navIdx = useNavIdx();
+
+  return (
+    <NavContainer>
+      <NavList toggle={showNav}>
+        <NavTab title={PageObj.Profile}></NavTab>
+        <NavTab title={PageObj.Tech}></NavTab>
+        <NavTab title={PageObj.Photo}></NavTab>
+        <NavTab title={PageObj.Diet}></NavTab>
+        <NavTab title={PageObj.Login}></NavTab>
+      </NavList>
+      <Indicator navIdx={navIdx} />
+    </NavContainer>
+  );
+}
+
+const NavContainer = styled.div`
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+`;
+
+const Indicator = styled.div<{ navIdx: number }>`
+  width: 80px;
+  display: ${({ navIdx }) => (navIdx >= 0 ? "block" : "none")};
+  margin-right: ${({ navIdx }) =>
+    navIdx >= 0 && `calc(320px - 80px * ${navIdx});`};
+  border: 1px solid ${Color.lightGray};
+  transition: all ease 0.5s 0s;
+  @media only screen and (max-width: ${MediaSize}px) {
+    display: none;
+  }
+`;
+
+const NavList = styled.ul<{ toggle: boolean }>`
+  justify-content: space-between;
+  display: flex;
+  align-items: flex-end;
+  width: 400px;
+  margin: 0;
+  padding: 0;
+  @media only screen and (max-width: ${MediaSize}px) {
+    display: ${({ toggle }) => (toggle ? "block" : "none")};
+    position: absolute;
+    top: ${HeaderHeight}px;
+    left: 0;
+    margin: 0;
+    background-color: white;
+    border-bottom: 1px solid black;
+    border-radius: 0 0 20px 20px;
+    width: 100%;
+    padding: 0;
+    background-color: ${(props) => props.theme.primary};
+    border-bottom: 1px solid ${(props) => props.theme.secondary};
+  }
+`;
