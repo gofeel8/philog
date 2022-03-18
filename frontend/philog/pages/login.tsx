848a3145183f1f9df9f147a3c9d4e8c29e7fa461
gofeel8 <gofeel8@hanmail.net>
2022-03-18 11:51:52 +0900

    Feat: 로그인 레이아웃 구현

--- frontend/philog/pages/login.tsx
+++ frontend/philog/pages/login.tsx
@@ -1,10 +1,89 @@
+import { FormEvent, useState } from "react";
+import styled from "styled-components";
 import Seo from "../components/Seo";
+import { Color } from "../utils/constant";
 
-export default function login() {
+export default function Login() {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const LoginHandler = (event: FormEvent) => {
+    console.log(id);
+    console.log(password);
+    event.preventDefault();
+  };
+
   return (
-    <div>
+    <Container>
       <Seo title="Philog" />
-      로그인
-    </div>
+      <LoginWrapper>
+        <form onSubmit={LoginHandler}>
+          <Label>
+            Admin ID:
+            <input
+              type="text"
+              name="id"
+              value={id}
+              onChange={({ target }) => setId(target.value)}
+            />
+          </Label>
+          <Label>
+            Password:
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </Label>
+          <Submit type="submit" value="Login" />
+        </form>
+      </LoginWrapper>
+    </Container>
   );
 }
+
+const Submit = styled.input`
+  position: relative;
+  margin-top: 10px;
+  width: 100%;
+  height: 30px;
+  border: 1px solid ${(props) => props.theme.secondary};
+  border-radius: 10px;
+  background-color: transparent;
+  color: ${(props) => props.theme.secondary};
+  cursor: pointer;
+  &:hover {
+    background-color: ${Color.lightGray};
+  }
+`;
+
+const Label = styled.label`
+  color: ${(props) => props.theme.secondary};
+  & input {
+    width: 100%;
+    margin-bottom: 20px;
+    margin-top: 10px;
+    border: none;
+    border-bottom: 1px solid ${(props) => props.theme.secondary};
+    background-color: transparent;
+    color: ${(props) => props.theme.secondary};
+    &:focus {
+      outline: none;
+    }
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 90vh;
+`;
+
+const LoginWrapper = styled.div`
+  margin-top: 100px;
+  width: 240px;
+  height: 240px;
+  padding: 30px;
+  border: 1px solid ${(props) => props.theme.secondary};
+  border-radius: 10px;
+`;
