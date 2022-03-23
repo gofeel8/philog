f0d8a5dffec2c4e0671960285a840c95bf18ddd7
gofeel8 <gofeel8@hanmail.net>
2022-03-24 00:25:28 +0900

    Feat: 게시글 상세조회 페이지 구현

--- frontend/philog/pages/tech/detail/[id].tsx
+++ frontend/philog/pages/tech/detail/[id].tsx
@@ -1,13 +1,16 @@
 import axios from "axios";
 import { GetServerSideProps } from "next";
-import { useEffect } from "react";
+import styled from "styled-components";
+import TechLayout from "../../../components/TechLayout";
+import "@toast-ui/editor/dist/toastui-editor.css";
+import { Viewer } from "@toast-ui/react-editor";
 
 interface IDetail {
   seq: number;
   category: string;
   title: string;
   content: string;
-  createdAt: Date;
+  createdAt: string;
 }
 
 export default function Detail({
@@ -17,7 +20,15 @@ export default function Detail({
   content,
   createdAt,
 }: IDetail) {
-  return <div>{content}</div>;
+  return (
+    <TechLayout title={title}>
+      <Container>
+        <PostTitle>{title}</PostTitle>
+        <PostDate>{new Date(createdAt).toLocaleString("ko-KR")}</PostDate>
+        <Viewer initialValue={content} />
+      </Container>
+    </TechLayout>
+  );
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
@@ -30,6 +41,19 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
       notFound: true,
     };
   }
-
   return { props: data };
 };
+
+const PostTitle = styled.div`
+  font-size: 30px;
+  padding: 10px;
+`;
+const PostDate = styled.div`
+  text-align: right;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
