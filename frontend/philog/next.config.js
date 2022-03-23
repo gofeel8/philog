c64620a80793ce8c683135245d4d30cfbced4dd6
gofeel8 <gofeel8@hanmail.net>
2022-03-23 17:43:11 +0900

    Feat: next redirect 사용

--- frontend/philog/next.config.js
+++ frontend/philog/next.config.js
@@ -11,6 +11,15 @@ const nextConfig = {
       ];
     }
   },
+  async redirects() {
+    return [
+      {
+        source: "/tech",
+        destination: "/tech/all",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
